0c04d72ec5946849fa3b0433cd8230d42f606b20
swathij869-collab <swathij869@gmail.com>
2026-07-23 20:56:11 +0530

    Create jewels and stones.java

---
+++ jewels and stones.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        HashSet<Character> set = new HashSet<>();
+
+        // Store all jewels
+        for (char c : jewels.toCharArray()) {
+            set.add(c);
+        }
+
+        int count = 0;
+
+        // Count jewels in stones
+        for (char c : stones.toCharArray()) {
+            if (set.contains(c)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
